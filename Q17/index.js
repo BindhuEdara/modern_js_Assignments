28de7012347ce8af16db210340061c61e76bef62
BindhuEdara <mandadi.bindu@gmail.com>
2025-11-23 12:10:42 +0530

    worked on Q17

---
+++ Q17/index.js
@@ -0,0 +1,34 @@
+arr1 = [1, 2, 3];
+arr2 = [4, 5];
+arr3 = [...arr1, ...arr2];
+console.log(arr3);
+
+console.log("-------------------");
+
+function sum(...nums) {
+  return nums.reduce((a, el) => {
+    return a + el;
+  }, 0);
+}
+console.log(sum(1, 2, 3, 4, 5));
+
+console.log("-------------------");
+
+const user = {
+  name: "Alice",
+  age: 22,
+  address: {
+    city: "Bangalore",
+    pin: 560001,
+  },
+};
+
+const {
+  name,
+  age,
+  address: { city, pin },
+} = user;
+
+console.log(name);
+console.log(city);
+console.log(pin);
